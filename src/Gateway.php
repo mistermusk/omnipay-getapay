46cfb4f80f80f570ec88809e5bc98a27974e4c33
RubbishCode <rubbishcode@gmail.com>
2023-12-20 00:14:26 +0300

    add isSignatureValid

--- src/Gateway.php
+++ src/Gateway.php
@@ -52,6 +52,35 @@ class Gateway extends AbstractGateway
         return $this->setParameter('secret_key_second', $value);
     }
 
+    public function isSignatureValid(array $callbackData, $first_level)
+    {
+        try {
+            $fieldsToSign = [
+                isset($callbackData['order_id']) ? $callbackData['order_id'] : '',
+                isset($callbackData['create_date']) ? $callbackData['create_date'] : '',
+                isset($callbackData['result_date']) ? $callbackData['result_date'] : '',
+                isset($callbackData['amount']) ? $callbackData['amount'] : '',
+                isset($callbackData['payment_id']) ? $callbackData['payment_id'] : '',
+                isset($callbackData['amount_gross']) ? $callbackData['amount_gross'] : '',
+                isset($callbackData['card_pan']) ? $callbackData['card_pan'] : '',
+                isset($callbackData['currency']) ? $callbackData['currency'] : '',
+                isset($callbackData['transaction_status']) ? $callbackData['transaction_status'] : '',
+                isset($callbackData['success']) ? ($callbackData['success'] ? 'true' : 'false') : ''
+            ];
+
+            $secret = $this->getSecretkeysecond();
+            if ($first_level){
+                $secret = $this->getSecretkey();
+            }
+            $dataString = implode('|', $fieldsToSign);
+            $computedSignature = hash_hmac('sha256', $dataString, $secret);
+            return $computedSignature === (isset($callbackData['signature']) ? $callbackData['signature'] : '');
+        } catch (Exception $e) {
+            return false;
+        }
+    }
+
+
     public function purchase(array $parameters = [])
     {
         return $this->createRequest('\Omnipay\Getapay\Message\PurchaseRequest', $parameters)
